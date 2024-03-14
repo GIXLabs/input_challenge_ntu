f47837ec20e610ea2dfe80805b327a0ecaee579f
Ashwin Nishad <ashwinnishad@Ashwins-MacBook-Air.local>
2024-03-14 16:27:37 -0700

    additional abstraction of functionality

--- src/code.py
+++ src/code.py
@@ -1,55 +1,27 @@
 # Import modules from standard library
-import time
 import board
-import usb_hid
 
 # Import Adafruit-specific libraries
-from adafruit_hid.keyboard import Keyboard
 from adafruit_hid.keycode import Keycode
 
 from input_code import InputCode
 
+ANALOG_INPUT_PIN = board.A0
+    
+button_assignments = {
+    board.GP2: Keycode.LEFT_ARROW,
+    board.GP3: Keycode.RIGHT_ARROW,
+    board.GP4: Keycode.UP_ARROW,
+    board.GP5: Keycode.SPACEBAR,
+}
+
 # Set up Pico pin ADC0 as an analog input
-pico = InputCode(board.A0)
+pico = InputCode(ANALOG_INPUT_PIN)
 
-# Determines if a potentiometer is connected
-potentiometer_present = pico.setup_potentiometer()
+# Setup the Potentiometer
+pico.setup_potentiometer()
 
-# Create a keyboard object that handles the USB protocol for sending keystrokes
-keyboard = Keyboard(usb_hid.devices)
-
-last_down = (
-    time.monotonic()
-)  # Set the most recent sending of the down key to the time of bootup
-spacing = 0  # This is the time between down presses (speed)
-
-# The following sets up all the pins and keyboard outputs associated with those pins
-buttons = []
-# The next 4 lines are the ones you will most likely want to edit
-buttons.append(pico.setup_button(board.GP2, Keycode.LEFT_ARROW))
-buttons.append(pico.setup_button(board.GP3, Keycode.RIGHT_ARROW))
-buttons.append(pico.setup_button(board.GP4, Keycode.UP_ARROW))
-buttons.append(pico.setup_button(board.GP5, Keycode.SPACEBAR))
+pico.setup_buttons(button_assignments)
 
 while True:
-    # Iterates through the list of buttons
-    for button in buttons:
-        button["OBJECT"].update()
-        if button["OBJECT"].fell:
-            print("Button pressed")
-            keyboard.send(button["KEYSTROKE"])
-
-    # Read the value from the potentiometer
-    pin_val = pico.return_analog_value()
-
-    if (
-        pin_val > 500 and potentiometer_present
-    ):  # Only true if the potentiometer has been turned a little and a potentiometer was detected on boot
-        # Scale the value from the pin linearly to change the spacing between down clicks (speed)
-        spacing = 65085 / (4 * pin_val - 1800)
-
-        # Compare the current time to the time the last down command was sent
-        # If enough time has elapsed send a rotation command and set the most recent rotation time to now
-        if time.monotonic() - last_down > spacing:
-            keyboard.send(Keycode.DOWN_ARROW)
-            last_down = time.monotonic()
+    pico.handle_button_presses()
